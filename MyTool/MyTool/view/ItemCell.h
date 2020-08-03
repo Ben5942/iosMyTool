b76f25a7ece44dc2f86cad3f1b71de7f96bf6a5a
张亚鹏 <787188834@qq.com>
2020-08-04 00:35:34 +0800

    no message

---
+++ MyTool/MyTool/view/ItemCell.h
@@ -0,0 +1,17 @@
+//
+//  ItemCell.h
+//  MyTool
+//
+//  Created by Y on 2020/8/4.
+//  Copyright © 2020 张亚鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ItemCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyTool/MyTool/view/ItemCell.m
@@ -0,0 +1,18 @@
+//
+//  ItemCell.m
+//  MyTool
+//
+//  Created by Y on 2020/8/4.
+//  Copyright © 2020 张亚鹏. All rights reserved.
+//
+
+#import "ItemCell.h"
+
+@implementation ItemCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
