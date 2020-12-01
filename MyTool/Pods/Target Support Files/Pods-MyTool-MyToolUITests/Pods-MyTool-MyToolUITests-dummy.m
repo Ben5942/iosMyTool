dd6a778142ee40619f550fa101f3f7b18dd337df
zhangyapeng <ypzhang@yhtech.co>
2020-12-01 11:40:01 +0800

    no message

---
+++ MyTool/Pods/Target Support Files/Pods-MyTool-MyToolUITests/Pods-MyTool-MyToolUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyTool_MyToolUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MyTool_MyToolUITests
+@end
---
+++ MyTool/Pods/Target Support Files/Pods-MyToolTests/Pods-MyToolTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyToolTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyToolTests
+@end
